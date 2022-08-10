e742ece3a1c361b7f5037ff9926f2d22318af5a5
pawel <pawelmiera@gmail.com>
2022-08-10 14:11:36 +0200

    init

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.uic import loadUi
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# import RPi.GPIO as gpio
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class FotoBudka(QDialog):
+    def __init__(self):
+        super(FotoBudka, self).__init__()
+        loadUi('fotobudka.ui', self)
+
+        self.setWindowTitle('Fotobudka')
+
+
+
+
+app = QApplication(sys.argv)
+widget = FotoBudka()
+widget.show()
+sys.exit(app.exec_())
