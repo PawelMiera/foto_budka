edc7ec0e92ca6af3acb15dba4526393ec6ca9900
pawel <pawelmiera@gmail.com>
2022-08-10 14:34:29 +0200

    IMAGE

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import sys
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot, QThread, pyqtSignal, Qt
 from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.uic import loadUi
+import cv2
 
 
 # import RPi.GPIO as gpio
+
+class ImageReader(QThread):
+    ImageUpdate = pyqtSignal(QImage)
+
+    def run(self):
+        self.ThreadActive = True
+        # Capture = cv2.VideoCapture(0)
+        while self.ThreadActive:
+            # ret, frame = Capture.read()
+            ret = True
+            frame = cv2.imread("0.jpg")
+            if ret:
+                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                ConvertToQtFormat = QImage(img.data, img.shape[1], img.shape[0], QImage.Format_RGB888)
+                img_qt = ConvertToQtFormat.scaled(1920, 1080, Qt.KeepAspectRatio)
+                self.ImageUpdate.emit(img_qt)
+
+    def stop(self):
+        self.ThreadActive = False
+        self.quit()
 
 
 class FotoBudka(QDialog):
@@ -16,7 +38,12 @@ class FotoBudka(QDialog):
 
         self.showFullScreen()
 
+        self.image_reader = ImageReader()
+        self.image_reader.ImageUpdate.connect(self.ImageViewUpdate)
+        self.image_reader.start()
 
+    def ImageViewUpdate(self, Image):
+        self.image_view.setPixmap(QPixmap.fromImage(Image))
 
 
 app = QApplication(sys.argv)
