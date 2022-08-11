01e5bfd3ecc54366a2f0a3328f03041c823e7717
pawel <pawelmiera@gmail.com>
2022-08-11 21:43:23 +0200

    many_changes

--- show_camera.py
+++ show_camera.py
@@ -3,7 +3,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
-# initialize the camera and grab a reference to the raw camera capture
+
 camera = PiCamera()
 camera.resolution = (1280, 720)
 camera.framerate = 32
@@ -12,7 +12,7 @@ rawCapture = PiRGBArray(camera, size=(1280, 720))
 time.sleep(0.1)
 # capture frames from the camera
 
-last_time = time.time
+last_time = time.time()
 id=0
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	start = time.time()
