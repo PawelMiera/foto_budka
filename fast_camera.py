7fa30719ed8ede7e36741bcbfe259e0f77564add
PawelMiera <pawelmiera@gmail.com>
2022-08-13 09:45:28 +0200

    camera speedup

--- fast_camera.py
+++ fast_camera.py
@@ -4,7 +4,7 @@ conn = cups.Connection()
 print(conn)
 printers = conn.getPrinters()
 print(printers)
-default_printer = printers.keys()[0]
+default_printer = list(printers.keys())[0]
 print(default_printer)
 cups.setUser('kidier')
 #conn.printFile(default_printer, self.image_reader.output_image_path, "boothy", {'fit-to-page': 'True'})
