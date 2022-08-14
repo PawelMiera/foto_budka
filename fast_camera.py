7a09aea4ee0bb6578a9826c62b851dd12b9331b8
PawelMiera <pawelmiera@gmail.com>
2022-08-14 11:44:35 +0200

    printing

--- fast_camera.py
+++ fast_camera.py
@@ -7,4 +7,4 @@ print(printers)
 default_printer = list(printers.keys())[0]
 print(default_printer)
 cups.setUser('kidier')
-conn.printFile(default_printer, "saved_images/7/print.png", "boothy", {'fit-to-page': 'False'})
+conn.printFile(default_printer, "saved_images/45/razem.png", "boothy", {'fit-to-page': 'True', 'media': "FotobudkaA6"})
